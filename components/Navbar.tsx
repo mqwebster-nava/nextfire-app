625942a6b43703654beaf5d82f5ea65c953ce3e8
mqwebster-nava <marqueswebster@navapbc.com>
2023-03-27 19:28:59 -0500

    navbar setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const user = null;
+  const username = null;
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* User is signed in and has a Username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* User is NOT signed in OR does not have a Username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}

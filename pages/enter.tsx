66e1864df68a1c706009e2983a38d4a8313d8a36
mqwebster-nava <marqueswebster@navapbc.com>
2023-03-28 22:39:32 -0500

    user sign up initially setup

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,39 @@
+import { auth, googleAuthProvider } from "@/lib/firebase";
+
 export default function Page({}) {
+  const user = null;
+  const username = null;
+
   return (
     <main>
       <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={SignInButton}>
+      <img src={`/google.png`} /> Sign In With Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {}
